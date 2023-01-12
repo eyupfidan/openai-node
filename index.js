84709091064f8cf9b535d776e6cdd2973d1c7a80
Eyüp Fidan <info@eyupfidan.com>
2023-01-12 15:13:45 +0300

    Add Cors Module

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 const configuration = new Configuration({
@@ -9,20 +16,21 @@ const configuration = new Configuration({
 
 app.get('/', (req, res) => {
   const openai = new OpenAIApi(configuration);
-
   async function runCompletion (prompt) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: prompt,
-    max_tokens: 1,
-  });
-  return await completion.data.choices[0].text;
+    max_tokens: 1000,
+  })
+    return await completion.data.choices[0].text;
   }
+  const {requestText} = req.query;
+  runCompletion(requestText).then(function(result) {
+    res.status(200).json(result)
+ })
   
- res.send( runCompletion('write a 500 words article for buy youtube subscribers keyword'))
 })
 
-
+app.use(cors(corsOptions)) 
 app.listen(port, () => {
-  console.log(`Example app listening on port localhost:${port}`)
 })
