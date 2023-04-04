4bf35bc36b4cf70d17d2dc10fc141ab66a7ab610
Eyüp Fidan <info@eyupfidan.com>
2023-04-04 11:26:41 +0300

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,25 +1,38 @@
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const getQuestion = ((req,res) => {
+const getQuestion = async (req, res) => {
+  const { requestText } = req.query;
+
+  try {
     const openai = new OpenAIApi(configuration);
-    async function runCompletion(prompt) {
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: prompt,
-            max_tokens: 1000,
-        })
-        return await completion.data.choices[0].text;
-    }
-    const { requestText } = req.query;
-    runCompletion(requestText).then(function (result) {
-        res.status(200).json(result)
-    })
-})
+
+    const result = await runCompletion(openai, requestText);
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const runCompletion = async (openai, prompt) => {
+  const { data } = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt,
+    max_tokens: 1000,
+  });
+
+  return data.choices[0].text;
+};
 
 module.exports = {
-    getQuestion,
-}
+  getQuestion,
+};
